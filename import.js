fd12fdfcb9b39187a2ca1a7bc41693a5221a33a7
pWhitS <patrickdwhitsell@gmail.com>
2016-02-01 19:42:56 -0500

    Statring key manager tab development. Imported a CSS3 menu. Mostly unaltered at this point.

---
+++ import.js
@@ -0,0 +1,29 @@
+
+var dPublicKey = 1;
+var dPrivateKey = 2;
+
+var gKeyType = dPublicKey;
+
+
+function showPublicInputFields() {
+	document.getElementById("pubkey-label").style.visibility = "visible";
+	document.getElementById("pubkey-name").style.visibility = "visible";
+	document.getElementById("rsakey").style.visibility = "visible";
+	document.getElementById("rsakey").style.height = "250px";
+	gKeyType = dPublicKey;
+}
+
+
+function showPrivateInputFields() {
+	document.getElementById("pubkey-label").style.visibility = "hidden";
+	document.getElementById("pubkey-name").style.visibility = "collapse";
+	document.getElementById("rsakey").style.visibility = "visible";
+	document.getElementById("rsakey").style.height = "250px";
+	gKeyType = dPrivateKey;
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById("import-pub").onclick = showPublicInputFields;  
+	document.getElementById("import-pri").onclick = showPrivateInputFields;
+});
