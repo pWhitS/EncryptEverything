1ba699e66ed35c8f71ccf61ff370425859def925
pWhitS <patrickdwhitsell@gmail.com>
2016-04-22 00:23:14 -0400

    Decryption works superficially, with a textfield holding the private key. Added 'sweetalert' to make it look cool

--- popup.js
+++ popup.js
@@ -36,19 +36,42 @@ function openKeyManagerTab() {
   });
 }
 
+function decryptSelectedText() {
+  var prikey = document.getElementById("sec").value;
+
+  getSelectedText(function(selectedText) {
+    var buf = selectedText.toString();    
+    var p = RSADecrypt(buf, prikey);
+    
+    if (p != false && p != "" && p != null) {
+      swal({
+        title: "",
+        text: "Decrypted Message",
+        type: "input",
+        inputValue: p,
+        closeOnConfirm: true
+      },
+      function(inval) {
+        return false;
+      });
+    }
+    else {
+      swal("Unable To Decrypt", "Something went wrong...", "error");
+    }
+
+  });
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("import").onclick = openKeyManagerTab;
+  document.getElementById("decrypt").onclick = decryptSelectedText;
 
   renderStatus("Initializing......");
 
-  var keygen = new JSEncrypt({default_key_size: 1024});
-
-  keygen.getKey();
-  prikey = keygen.getPrivateKey();
-  pubkey = keygen.getPublicKey();
+  // var keygen = new JSEncrypt({default_key_size: 1024});
+  // keygen.getKey();
+  // prikey = keygen.getPrivateKey();
+  // pubkey = keygen.getPublicKey();
   
-  getSelectedText(function(selectedText) {
-    var buf = selectedText.toString();    
-
-  });
 });
